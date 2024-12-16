8824612d5ff67b5580674bab8e558dc76fce1d1f
Giansanti Andrea (DS&G) <andrea.giansanti@enel.com>
2024-12-17 00:34:40 +0100

    removed callbacks folder

--- dash_project_name/layouts/homepage.py
+++ dash_project_name/layouts/homepage.py
@@ -1,6 +1,7 @@
 """ aaa """
 
-from dash import dash_table, dcc, html
+import plotly.express as px
+from dash import Input, Output, callback, dash_table, dcc, html
 
 from dash_project_name.dao.csv_reader import sample_data_df
 
@@ -16,3 +17,14 @@ homepage_layout = [
     dash_table.DataTable(data=sample_data_df.to_dict("records"), page_size=10),
     dcc.Graph(figure={}, id="controls-and-graph"),
 ]
+
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id="controls-and-graph", component_property="figure"),
+    Input(component_id="controls-and-radio-item", component_property="value"),
+)
+def update_graph(col_chosen):
+    """aaa"""
+    fig = px.histogram(sample_data_df, x="continent", y=col_chosen, histfunc="avg")
+    return fig
